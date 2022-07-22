c36bd3a0966b3f92f5f4c780eac809debeb51564
MrRedHand <morrag1986@gmail.com>
2022-07-22 20:42:57 +0300

    Revert "accreditation tab is ready"
    This reverts commit 48ce5acf7452c7125282dcd2017239b20b5569f4.

--- src/services/to-server-requests.test.js
+++
@@ -1,7 +0,0 @@
-import {getFullData} from "./to-server-requests";
-
-describe('Получена ли вся первичная дата с сервера', () => {
-    it('Должен прийти массив ингредиентов', () => {
-        expect(getFullData())
-    });
-})
